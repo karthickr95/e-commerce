6d1fe93f3906d60ec93d699273ae4d990e1234bf
Karthick Ramanathan <30870272+karthickramanathan95@users.noreply.github.com>
2020-08-09 15:42:22 +0530

    Server files changed to javascript

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import path from 'path'
-import Stripe from 'stripe'
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
     apiVersion: '2020-03-02',
 })
 
 const PORT = process.env.PORT || 5000
 
-const app: Application = express()
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -22,7 +21,7 @@ app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('*', (req: Request, res: Response) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
@@ -32,14 +31,8 @@ app.listen(PORT, () => {
     console.log('Press CTRL-C to stop\n')
 })
 
-type PaymentRequestBody = {
-    amount: number,
-    token: {
-        id: string
-    }
-}
 
-app.post('/payment', (req: Request<{}, {}, PaymentRequestBody>, res: Response) => {
+app.post('/payment', (req, res) => {
     const body = {
         source: req.body.token.id,
         amount: req.body.amount,
